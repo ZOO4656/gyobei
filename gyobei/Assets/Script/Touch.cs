872b11197a3500c62fc4cebb6cb5f63f0581f493
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-08-17 16:42:37 +0900

    TouchScreenにタッチ判定を付与

---
+++ gyobei/Assets/Script/Touch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if (Input.GetButtonDown("Fire1")) {
+		// 	Debug.Log("PushMouseButtn");
+		// }	//入力の検知
+		if (Input.GetMouseButtonDown(0)) {
+		    Debug.Log("クリックした瞬間");
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+		    Debug.Log("離した瞬間");
+		}
+
+		if (Input.GetMouseButton(0)) {
+		    Debug.Log("クリックしっぱなし");
+		}
+
+	}
+
+}
