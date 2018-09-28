f12fe8ce31462e90fa92c59b04a466bfcee6979b
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-09-28 10:27:16 +0900

    クリックで魚のポジションUp

---
+++ gyobei/Assets/Script/Fishing.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishing : MonoBehaviour {
+	
+	//todo:"fish"タグを持つオブジェクトを格納する箇所を作成する。
+	public GameObject fish;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//
+		if (Input.GetMouseButtonDown(0)){ FishDown();}
+		if (Input.GetMouseButtonUp(0)){ FishOn();}
+		if (Input.GetMouseButton(0)){ FishHold	();}
+	}
+
+	//todo:15 スマホ用のタッチ処理についても追加する
+
+	//釣り竿を投げる
+	public void FishDown () {
+		Throw();
+	}
+
+	//釣り竿を引き上げる
+	public void FishOn () {
+		Pullin();
+
+	}
+
+	//釣り竿をホールド・魚が暴れている
+	public void FishHold () {
+		Debug.Log("魚が暴れている");
+	}
+
+	//釣り竿を投げた時の処理
+	public void Throw () {
+		Debug.Log("釣り竿を投げた");
+
+	}
+
+	//魚を釣り上げた時の処理
+	public void Pullin() {
+		Debug.Log("魚を釣った");
+		Transform mypos = fish.transform;
+		Vector3 pos = mypos.position;
+		pos.y += 1f;
+		mypos.position = pos;
+
+		// fishpos.transform.position = new Vectore3(fishpos.x, fishpos.y + 10f,fishpos.z);
+		// fish.transform.position = fishpos.transform.position;
+	}
+
+}
---
+++ gyobei/Assets/Script/TouchDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDictionary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+		    Debug.Log("図鑑をクリックした瞬間");
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+		    Debug.Log("図鑑を離した瞬間");
+		}
+
+		if (Input.GetMouseButton(0)) {
+		    Debug.Log("図鑑をクリックしっぱなし");
+		}
+	}
+
+		
+}
