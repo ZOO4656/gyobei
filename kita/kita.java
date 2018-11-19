268b2b30457b15312e984437ee6d4ce43ec59326
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-19 11:20:34 +0900

    練習1-6

--- kita/kita.java
+++ kita/kita.java
@@ -1,5 +1,11 @@
 public class kita{
 	public static void main( String[] args ) {
-		System.out.println(13*17);
+		int x = 7;
+		x *= 3;
+		System.out.println(x);
+
+		x /= 2;
+		System.out.println(x);
+
 	}
 }
