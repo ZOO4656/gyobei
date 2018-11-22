d6a7a04f51e9a9aea0b9f5cca31c32d22c8753aa
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-22 09:31:43 +0900

    練習2-2

--- kita/kita.java
+++ kita/kita.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		String s = text.readLine();
-		System.out.println(s);
+		int x = Integer.parseInt(text.readLine());
+		System.out.println(x);
 	}
 }
