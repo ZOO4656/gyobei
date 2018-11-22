94ae89322fbd1755395ceaa574abadaada922d25
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-22 11:08:53 +0900

    練習2-6

--- kita/kita.java
+++ kita/kita.java
@@ -3,8 +3,9 @@ import java.io.*;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("年齢を入力してください");
 		int x = Integer.parseInt(text.readLine());
-		int y = Integer.parseInt(text.readLine());
-		System.out.println((x+y)/2);
+		System.out.println("あなたが生まれてからのおおよその日数は"+x*365+"日です");
+
 	}
 }
