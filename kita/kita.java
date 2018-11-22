a9f3436171bf83e88194f41a704523b0ab853912
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-22 09:50:10 +0900

    練習2-4

--- kita/kita.java
+++ kita/kita.java
@@ -4,8 +4,11 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.parseInt(text.readLine());
-		System.out.println(x);
-		System.out.println(x*x);
-		System.out.println(x*x*x);
+		int y = Integer.parseInt(text.readLine());
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
 	}
 }
