a34cd3266adb560c8fb5561f84ec1b1c452bc240
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-24 17:15:41 +0900

    練習3-11

--- kita/kita.java
+++ kita/kita.java
@@ -3,31 +3,20 @@ import java.io.*;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("曜日を入力してください。0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜");
+		System.out.println("何月か入力してください。0=1月,1=2月,2=3月,3=4月,4=5月,5=7月,6=9月,7=10月,8=11月,9=12月");
 		int x = Integer.parseInt(text.readLine());
 
-		System.out.println("来院時間を入力してください。0=午前、1=午後、2=夜間");
-		int y = Integer.parseInt(text.readLine());
-
-		if(x < y && x % 2 == 0 && y % 2 == 0) {
-			System.out.println( "xはyより小さく、かつ、xとyは共に偶数である。" );
-		}
-
-		if(x == y && x < 0) {
-		    System.out.println( "xとyは等しく、かつ、負の数である。" );
-		}
-
-		if(x < y || x % 2 ==0) {
-	    	System.out.println( "xはyより小さい、または、xは偶数である。" );
-		}
-
-		if(x <= 10 || x >= 100 && y >= 10 && y <= 100) {
-	    	System.out.println( "xは10以下または100以上で、かつ、yは10以上かつ100以下である。" );
-		}
-
-		if(!(x < 0 && y < 0) {
-	    	System.out.println( "xもyも負の数である、ではない。" );
-		}
+		switch(x) {
+			case 0:System.out.println("元日、成人の日");
+			case 1:System.out.println("建国記念の日");
+			case 2:System.out.println("春分の日");
+			case 3:System.out.println("昭和の日");
+			case 4:System.out.println("憲法記念日、みどりの日、こどもの日");
+			case 5:System.out.println("海の日");
+			case 6:System.out.println("敬老の日、秋分の日");
+			case 7:System.out.println("体育の日");
+			case 8:System.out.println("化の日、勤労感謝の日");
+			case 9:System.out.println("天皇誕生日");
 		}
 	}
 }
