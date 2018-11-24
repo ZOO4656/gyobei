f27062999d78045e14a72e235114ca6eba96f156
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-24 17:28:37 +0900

    練習3-12

--- kita/kita.java
+++ kita/kita.java
@@ -3,20 +3,18 @@ import java.io.*;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("何月か入力してください。0=1月,1=2月,2=3月,3=4月,4=5月,5=7月,6=9月,7=10月,8=11月,9=12月");
+		System.out.println("好きな寿司ネタを入力してください。0=さば,1=サバ,2=鯖,3=saba,4=sava,5=
+mackerel,6=,鲭鱼");
 		int x = Integer.parseInt(text.readLine());
 
 		switch(x) {
-			case 0:System.out.println("元日、成人の日");
-			case 1:System.out.println("建国記念の日");
-			case 2:System.out.println("春分の日");
-			case 3:System.out.println("昭和の日");
-			case 4:System.out.println("憲法記念日、みどりの日、こどもの日");
-			case 5:System.out.println("海の日");
-			case 6:System.out.println("敬老の日、秋分の日");
-			case 7:System.out.println("体育の日");
-			case 8:System.out.println("化の日、勤労感謝の日");
-			case 9:System.out.println("天皇誕生日");
+			case 0:System.out.println("大吉");break;
+			case 1:System.out.println("中吉");break;
+			case 2:System.out.println("吉");break;
+			case 3:System.out.println("半吉");break;
+			case 4:System.out.println("末吉");break;
+			case 5:System.out.println("小吉");break;
+			case 6:System.out.println("凶");break;
 		}
 	}
 }
