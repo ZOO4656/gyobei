ecf29a1bf51af8bd12966dfcea5a5005b82673b1
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-24 15:51:46 +0900

    練習3-1

--- kita/kita.java
+++ kita/kita.java
@@ -3,9 +3,12 @@ import java.io.*;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("年齢を入力してください");
+		System.out.println("変数xに入れる値を入力してください");
 		int x = Integer.parseInt(text.readLine());
-		System.out.println("あなたが生まれてからのおおよその日数は"+x*365+"日です");
 
+		System.out.println("変数yに入れる値を入力してください");
+		int y = Integer.parseInt(text.readLine());
+
+		if(x > y) {System.out.println("xはyより大きい。");}
 	}
 }
