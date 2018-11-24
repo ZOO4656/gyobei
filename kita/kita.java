d13f9cb52d2a914c28d2915b9bb945fe6627cd13
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-24 16:58:58 +0900

    練習3-10

--- kita/kita.java
+++ kita/kita.java
@@ -9,14 +9,25 @@ public class kita{
 		System.out.println("来院時間を入力してください。0=午前、1=午後、2=夜間");
 		int y = Integer.parseInt(text.readLine());
 
-		if(y == 0 && x == 0||x ==2||x ==4) {
-			System.out.println("休診");
-		}else if(y == 1 && x == 0 || x == 6) {
-			System.out.println("休診");
-		}else if(y == 2 && x == 0||x == 3||x == 6) {
-			System.out.println("休診");
-		}else{
-			System.out.println("○");
+		if(x < y && x % 2 == 0 && y % 2 == 0) {
+			System.out.println( "xはyより小さく、かつ、xとyは共に偶数である。" );
+		}
+
+		if(x == y && x < 0) {
+		    System.out.println( "xとyは等しく、かつ、負の数である。" );
+		}
+
+		if(x < y || x % 2 ==0) {
+	    	System.out.println( "xはyより小さい、または、xは偶数である。" );
+		}
+
+		if(x <= 10 || x >= 100 && y >= 10 && y <= 100) {
+	    	System.out.println( "xは10以下または100以上で、かつ、yは10以上かつ100以下である。" );
+		}
+
+		if(!(x < 0 && y < 0) {
+	    	System.out.println( "xもyも負の数である、ではない。" );
+		}
 		}
 	}
 }
