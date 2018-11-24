0c8326d8ac2c63408b47c7a6ef0ca3c62e266c6a
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-24 17:34:55 +0900

    練習3-13

--- kita/kita.java
+++ kita/kita.java
@@ -3,18 +3,23 @@ import java.io.*;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("好きな寿司ネタを入力してください。0=さば,1=サバ,2=鯖,3=saba,4=sava,5=
-mackerel,6=,鲭鱼");
+		System.out.println("月日を入力してください。0=１月,1=２月,2=３月,3=４月,4=５月,5=６月,6=７月,7=８月,8=９月,9=10月,10=11月,11=12月");
 		int x = Integer.parseInt(text.readLine());
 
 		switch(x) {
-			case 0:System.out.println("大吉");break;
-			case 1:System.out.println("中吉");break;
-			case 2:System.out.println("吉");break;
-			case 3:System.out.println("半吉");break;
-			case 4:System.out.println("末吉");break;
-			case 5:System.out.println("小吉");break;
-			case 6:System.out.println("凶");break;
+			case 0:System.out.println("31日です");break;
+			case 1:System.out.println("２８日です");break;
+			case 2:System.out.println("30日です");break;
+			case 3:System.out.println("31日です");break;
+			case 4:System.out.println("30日です");break;
+			case 5:System.out.println("31日です");break;
+			case 6:System.out.println("30日です");break;
+			case 7:System.out.println("31日です");break;
+			case 8:System.out.println("30日です");break;
+			case 9:System.out.println("31日です");break;
+			case 10:System.out.println("30日です");break;
+			case 11:System.out.println("31日です");break;
+			default:System.out.println("入力が間違っています");break;
 		}
 	}
 }
