4f2aac1114006a17c0d84e9be36162c04f5aae74
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-11-25 16:13:29 +0900

    アセットを追加

---
+++ gyobei/Assets/Script/BackButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour {
+
+public GameObject currentDisplayt;
+
+	public void OnClick() {
+		currentDisplayt.SetActive(false);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
