cf1c8265fbad9a68c7b4066839d3b34c3a20ab49
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 17:44:22 +0900

    練習4-10

--- kita/kita.java
+++ kita/kita.java
@@ -4,17 +4,10 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
-		int max = 0;
-		int min = 0;
-		for(int i = 1; i <= 10; i++) {
-				int x = Integer.parseInt(text.readLine());
-			if (max < x) {
-				max = x;
-			}
-			if(min > x) {
-				min = x;
-			}
+			System.out.println("数字を入力してください");
+			int x = Integer.parseInt(text.readLine());
+		for(int i = 1; i <= x; i++) {
+				System.out.println("*");
 		}
-		System.out.println("最大数値は" + max + "で最小値は"+min);
 	}
 }
