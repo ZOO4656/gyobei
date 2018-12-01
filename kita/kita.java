d071f74e725c2ce516fae1a9ff018674438f7a0c
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 17:40:04 +0900

    練習4-9

--- kita/kita.java
+++ kita/kita.java
@@ -5,12 +5,16 @@ public class kita{
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
 		int max = 0;
+		int min = 0;
 		for(int i = 1; i <= 10; i++) {
 				int x = Integer.parseInt(text.readLine());
 			if (max < x) {
 				max = x;
 			}
+			if(min > x) {
+				min = x;
+			}
 		}
-		System.out.println("最大数値は" + max);
+		System.out.println("最大数値は" + max + "で最小値は"+min);
 	}
 }
