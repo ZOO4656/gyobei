861224a56cedbae3c42ec65404b53d9b62852caa
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 18:19:36 +0900

    練習4-12

--- kita/kita.java
+++ kita/kita.java
@@ -4,15 +4,15 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
+			int t = 0;
+		for(int i = 1; t <= 100; i++) {
 			System.out.println("数字を入力してください");
 			int x = Integer.parseInt(text.readLine());
-			int t = 0;
-		for(int i = 1; i <= x; i++) {
-			t ++;
-			if(t >= 10){
-				t = 0;
+			t += x;
+			if(t >= 100){
+				break;
 			}
-				System.out.println(t);
 		}
+			System.out.println(t);
 	}
 }
