1b1c0a593f3eb74956a04033811a4ed93ead497e
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 17:19:49 +0900

    練習4-8

--- kita/kita.java
+++ kita/kita.java
@@ -4,22 +4,13 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
-		int kyozin = 0;
-		int hanshin = 0;
-		for(int i = 1; i <= 9; i++) {
-			for(int j = 0; j <= 0; j++) {
-			System.out.println(i + "回表巨人の得点は？");
+		int max = 0;
+		for(int i = 1; i <= 10; i++) {
 				int x = Integer.parseInt(text.readLine());
-				kyozin += x;
+			if (max < x) {
+				max = x;
 			}
-			System.out.println(i + "回裏阪神の得点は？");
-				int x = Integer.parseInt(text.readLine());
-				hanshin += x;
-			}
-		if (kyozin > hanshin) {
-			System.out.println("巨人の勝ち");
-		}else{
-			System.out.println("阪神の勝ち");
 		}
+		System.out.println("最大数値は" + max);
 	}
 }
