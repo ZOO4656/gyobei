9030651247c2c1411a5327a5d8e7b6ade25296af
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 18:46:07 +0900

    練習4-14

--- kita/kita.java
+++ kita/kita.java
@@ -7,15 +7,18 @@ public class kita{
 			int strike = 0;
 			int ball = 0;
 		for(int i = 1; i <= 10; i++) {
-			System.out.println("ストライク= 0 ボール= 1？");
+			System.out.println("ストライク= 0 ボール= 1 ファウル= 3？");
 			int x = Integer.parseInt(text.readLine());
-			if(x == 0){
+			if(x == 0 ){
 				strike +=1;
 			}else if(x == 1){
 				ball +=1;
 			}
+			if(x == 3 && strike < 2) {
+				strike +=1;
+			}
 
-			if(strike == 3 || ball ==4){
+			if(strike == 3 || ball == 4){
 				break;
 			}
 		}
