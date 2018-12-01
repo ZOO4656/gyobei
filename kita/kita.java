ef8de827cddbead1967a9781246fbb64786b0335
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 17:15:44 +0900

    練習4-7

--- kita/kita.java
+++ kita/kita.java
@@ -4,17 +4,22 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
-		int win = 0;
-		int lose = 0;
-		for(int i = 1; i <= 10; i++) {
-			System.out.println("0か1の数字を入力してください。");
-			int x = Integer.parseInt(text.readLine());
-			if(x == 0) {
-				lose += 1;
-			}else{
-				win += 1;
+		int kyozin = 0;
+		int hanshin = 0;
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 0; j <= 0; j++) {
+			System.out.println(i + "回表巨人の得点は？");
+				int x = Integer.parseInt(text.readLine());
+				kyozin += x;
 			}
+			System.out.println(i + "回裏阪神の得点は？");
+				int x = Integer.parseInt(text.readLine());
+				hanshin += x;
+			}
+		if (kyozin > hanshin) {
+			System.out.println("巨人の勝ち");
+		}else{
+			System.out.println("阪神の勝ち");
 		}
-			System.out.println("勝ちの総数は" +win+ "負けの総数は"+lose);
 	}
 }
