0634b2d89e99016f27282358b39965f6d3a6a03e
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 19:29:19 +0900

    練習4-18

--- kita/kita.java
+++ kita/kita.java
@@ -4,24 +4,16 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
-			int strike = 0;
-			int ball = 0;
-		for(int i = 1; i <= 10; i++) {
-			System.out.println("ストライク= 0 ボール= 1 ファウル= 3？");
+			int sum = 0;
+		for(int i = 1; i > 0; i++) {
+			System.out.println("数字を入力してください。ただし0を入力すると計算が終了します。");
 			int x = Integer.parseInt(text.readLine());
-			if(x == 0 ){
-				strike +=1;
-			}else if(x == 1){
-				ball +=1;
-			}
-			if(x == 3 && strike < 2) {
-				strike +=1;
-			}
-
-			if(strike == 3 || ball == 4){
+			if(x != 0 ){
+				sum +=x;
+			}else if(x == 0) {
 				break;
 			}
 		}
-			System.out.println(strike + "ストライク, " + ball + "ボール");
+			System.out.println(sum);
 	}
 }
