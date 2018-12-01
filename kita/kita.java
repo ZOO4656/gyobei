81ad612c98afdbc4f27b6e2970d070f8e96ebd38
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-01 16:31:13 +0900

    練習4-2

--- kita/kita.java
+++ kita/kita.java
@@ -4,8 +4,8 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 			// int x = Integer.parseInt(text.readLine());
-		for(int i = 0; i <= 10; i++) {
-			System.out.println(i + "回目のSAMP");
+		for(int i = 0; i <= 9; i++) {
+			System.out.println(i * 3);
 		}
 	}
 }
