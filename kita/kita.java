c979ad008228fdfcbdc407045b870c099b6e9322
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-02 12:33:34 +0900

    練習問題

--- kita/kita.java
+++ kita/kita.java
@@ -1,18 +1,38 @@
 import java.io.*;
+import java.util.Random;
 
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-			String sum = "#";
-			int counter = 1;
-			System.out.println("数字を入力してください。");
+		boolean playerTurn = true;
+		int num = 0;
+		while (num <= 19) {
+			System.out.println("20を言ったら負けゲーム!! 現在の数字は"+ num +"です。 数字を入力してください。");
+			playerTurn = true;
 			int x = Integer.parseInt(text.readLine());
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < counter; j++){
-				System.out.print(sum);
+			if(x == 0 || x >= 4){
+				System.out.println("1~3の数字を入力してください");
+				continue;
 			}
-				counter ++;
-				System.out.println();
+			num += x;
+			if(num >= 20){
+				break;
+			}
+			System.out.println("コンピューターのターン");
+			playerTurn = false;
+			Random ran = new Random();
+			int dom = ran.nextInt(3) + 1;
+			System.out.println("コンピューターの数字は"+(dom));
+			num += dom;
+			System.out.println("現在の数字は"+num+"です。");
+			if(num >= 20){
+				break;
+			}
+		}
+		if(playerTurn == true) {
+			System.out.println("ゲームの勝者はコンピュータです。");
+		}else{
+			System.out.println("ゲームの勝者はプレイヤーです。");
 		}
 	}
 }
