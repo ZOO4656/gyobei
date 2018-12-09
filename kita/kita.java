88535d2e6f708828b07ce2ebc0b6c90c1b3ed227
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-09 11:03:02 +0900

    練習4ー15

--- kita/kita.java
+++ kita/kita.java
@@ -4,35 +4,19 @@ import java.util.Random;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		boolean playerTurn = true;
-		int num = 0;
-		while (num <= 19) {
-			System.out.println("20を言ったら負けゲーム!! 現在の数字は"+ num +"です。 数字を入力してください。");
-			playerTurn = true;
-			int x = Integer.parseInt(text.readLine());
-			if(x == 0 || x >= 4){
-				System.out.println("1~3の数字を入力してください");
-				continue;
+		System.out.println("数字を入力してください。それが素数かどうか当てましょう");
+		int x = Integer.parseInt(text.readLine());
+		for(int j = 2; j <= 1000; j++) {
+			boolean is_sosu = true;
+			for(int i = 2; i < j/2; i++) {
+				if(j % i == 0) {
+					is_sosu = false;
+					break;
+				}
 			}
-			num += x;
-			if(num >= 20){
-				break;
+			if(is_sosu) {
+				System.out.println(j);
 			}
-			System.out.println("コンピューターのターン");
-			playerTurn = false;
-			Random ran = new Random();
-			int dom = ran.nextInt(3) + 1;
-			System.out.println("コンピューターの数字は"+(dom));
-			num += dom;
-			System.out.println("現在の数字は"+num+"です。");
-			if(num >= 20){
-				break;
-			}
-		}
-		if(playerTurn == true) {
-			System.out.println("ゲームの勝者はコンピュータです。");
-		}else{
-			System.out.println("ゲームの勝者はプレイヤーです。");
 		}
 	}
 }
