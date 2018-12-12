b75453cbaeaecec3b4aac418069691d197a740f5
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-13 06:37:27 +0900

    練習4-21

--- kita/kita.java
+++ kita/kita.java
@@ -4,19 +4,24 @@ import java.util.Random;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("数字を入力してください。それが素数かどうか当てましょう");
+		System.out.println("数字を入力してください。");
+		int counter_x = 1;
+		String peke = "x";
+
 		int x = Integer.parseInt(text.readLine());
-		for(int j = 2; j <= 1000; j++) {
-			boolean is_sosu = true;
-			for(int i = 2; i < j/2; i++) {
-				if(j % i == 0) {
-					is_sosu = false;
-					break;
+		int reverse_counter = x;
+
+		for(int i = 0; i < x; i++) {
+			for(int j = 0; j <= x ; j++) {
+				if(j == counter_x ||j == reverse_counter) {
+					System.out.print(peke);
+				}else{
+					System.out.print(" ");
 				}
 			}
-			if(is_sosu) {
-				System.out.println(j);
-			}
+			counter_x ++;
+			reverse_counter --;
+			System.out.println();
 		}
 	}
 }
