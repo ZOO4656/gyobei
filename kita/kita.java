32abd27a25854dabbd245152129c4a63dc22d4d9
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-15 17:12:53 +0900

    練習5-3

--- kita/kita.java
+++ kita/kita.java
@@ -7,7 +7,11 @@ public class kita{
 
 		int inputNumberHolder = 0;
 		int reverseNumber = 0;
+		int chou_counter = 0;
+		int han_counter = 0;
 		int[] countNumber = new int[10];
+		int[] chou = new int[10];	//偶数
+		int[] han = new int[10];	//奇数
 		while (inputNumberHolder <= 9) {
 			System.out.println(inputNumberHolder+1+"目の数字を入力してください。");
 			int x = Integer.parseInt(text.readLine());
@@ -15,9 +19,24 @@ public class kita{
 			inputNumberHolder ++;
 			reverseNumber = inputNumberHolder;
 		}
-		for(int i = 0; i < inputNumberHolder; i++ ) {
-			reverseNumber --;
-			System.out.println(reverseNumber+1+"番目の数字は"+countNumber[reverseNumber]+"です。");
+		for(int i = 0; i < inputNumberHolder; i++) {
+			if(countNumber[i] % 2 == 0) {
+				chou[chou_counter] = countNumber[i];
+				chou_counter++;
+			}else{
+				han[han_counter] = countNumber[i];
+				han_counter++;
+			}
 		}
+		System.out.print("偶数");
+		for(int i = 0; i < chou_counter; i++) {
+			System.out.print(chou[i]+",");
+		}
+		System.out.println();
+		System.out.print("奇数");
+		for(int i = 0; i < han_counter; i++) {
+			System.out.print(han[i]+",");
+		}
+
 	}
 }
