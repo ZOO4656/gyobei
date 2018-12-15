14e1b2a07a399e90cb2c1fe3ea025ff1c861fe62
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-15 17:46:45 +0900

    練習5-5

--- kita/kita.java
+++ kita/kita.java
@@ -4,27 +4,14 @@ import java.util.Random;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-
-		int inputNumberHolder = 0;
-		int reverseNumber = 0;
-		int counter = 0;
-		int max_Observer = 0;
-		int[] countNumber = new int[10];
-		while (inputNumberHolder <= 9) {
-			System.out.println(inputNumberHolder+1+"目の数字を入力してください。");
-			int x = Integer.parseInt(text.readLine());
-			countNumber[inputNumberHolder] = x;
-			inputNumberHolder ++;
-			counter = inputNumberHolder;
-			for(int i = 0; i < inputNumberHolder; i++) {
-				max_Observer += countNumber[i];
-			}
-			if(max_Observer >= 100) {break;}
-			max_Observer = 0;
-		}
-
-		for(int i = 0; i <inputNumberHolder; i++) {
-			System.out.print(countNumber[i]+",");
+		int kuku[][] = new int[9][9];
+		// 配列 kuku に値を代入する
+		for( int i = 0 ; i < 9 ; i++) {
+	   	 	for( int j = 0 ; j < 9 ; j++) {
+	   	 		kuku[i][j] = (i+1)*(j+1);
+   	    		System.out.printf( " %2d", kuku[i][j]);
+ 		   		System.out.println();
+ 			}
 		}
 	}
 }
