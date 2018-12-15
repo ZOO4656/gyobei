5ccf8defb7cb73f6d94dd8707d5d8c7939ba9fa4
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-15 17:28:43 +0900

    練習5-4

--- kita/kita.java
+++ kita/kita.java
@@ -7,36 +7,24 @@ public class kita{
 
 		int inputNumberHolder = 0;
 		int reverseNumber = 0;
-		int chou_counter = 0;
-		int han_counter = 0;
+		int counter = 0;
+		int max_Observer = 0;
 		int[] countNumber = new int[10];
-		int[] chou = new int[10];	//偶数
-		int[] han = new int[10];	//奇数
 		while (inputNumberHolder <= 9) {
 			System.out.println(inputNumberHolder+1+"目の数字を入力してください。");
 			int x = Integer.parseInt(text.readLine());
 			countNumber[inputNumberHolder] = x;
 			inputNumberHolder ++;
-			reverseNumber = inputNumberHolder;
-		}
-		for(int i = 0; i < inputNumberHolder; i++) {
-			if(countNumber[i] % 2 == 0) {
-				chou[chou_counter] = countNumber[i];
-				chou_counter++;
-			}else{
-				han[han_counter] = countNumber[i];
-				han_counter++;
+			counter = inputNumberHolder;
+			for(int i = 0; i < inputNumberHolder; i++) {
+				max_Observer += countNumber[i];
 			}
-		}
-		System.out.print("偶数");
-		for(int i = 0; i < chou_counter; i++) {
-			System.out.print(chou[i]+",");
-		}
-		System.out.println();
-		System.out.print("奇数");
-		for(int i = 0; i < han_counter; i++) {
-			System.out.print(han[i]+",");
+			if(max_Observer >= 100) {break;}
+			max_Observer = 0;
 		}
 
+		for(int i = 0; i <inputNumberHolder; i++) {
+			System.out.print(countNumber[i]+",");
+		}
 	}
 }
