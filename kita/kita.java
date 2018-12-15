9ac60357cdb66f5ead95f27e8b6926d78d28798d
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-15 16:20:47 +0900

    練習5-2

--- kita/kita.java
+++ kita/kita.java
@@ -6,16 +6,18 @@ public class kita{
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 
 		int inputNumberHolder = 0;
+		int reverseNumber = 0;
 		int[] countNumber = new int[10];
 		while (inputNumberHolder <= 9) {
 			System.out.println(inputNumberHolder+1+"目の数字を入力してください。");
 			int x = Integer.parseInt(text.readLine());
 			countNumber[inputNumberHolder] = x;
 			inputNumberHolder ++;
+			reverseNumber = inputNumberHolder;
 		}
-		for(int i = 0; i < 10; i++) {
-			System.out.println((i+1)+"番目の数字を二倍した数字は"+countNumber[i]*2+"です。");
+		for(int i = 0; i < inputNumberHolder; i++ ) {
+			reverseNumber --;
+			System.out.println(reverseNumber+1+"番目の数字は"+countNumber[reverseNumber]+"です。");
 		}
-
 	}
 }
