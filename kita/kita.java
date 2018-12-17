dd514f4d8bef9164775fe72fa9927fa4c53eb121
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-17 11:16:28 +0900

    練習5-6

--- kita/kita.java
+++ kita/kita.java
@@ -1,16 +1,15 @@
 import java.io.*;
 import java.util.Random;
 
-public class kita{
+public class Kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
 		int kuku[][] = new int[9][9];
-		// 配列 kuku に値を代入する
+		//配列 kuku に値を代入する
 		for( int i = 0 ; i < 9 ; i++) {
 	   	 	for( int j = 0 ; j < 9 ; j++) {
 	   	 		kuku[i][j] = (i+1)*(j+1);
-   	    		System.out.printf( " %2d", kuku[i][j]);
- 		   		System.out.println();
+	   	 		System.out.println(kuku[i][j]);
  			}
 		}
 	}
