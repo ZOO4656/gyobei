22a2596b38f5be6c1ffaf1abdbbdf83ea6029a32
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-21 11:14:51 +0900

    練習6-2

--- kita/kita.java
+++ kita/kita.java
@@ -3,12 +3,11 @@ import java.util.Random;
 
 public class Kita{
 	public static void main( String[] args ) throws IOException {
-		System.out.println("数字を入力してください。その数字を2乗します。");
 		BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(nijo(5));
+		System.out.println(average(5,7));
 
 	}
-		public static int nijo(int x) {
-			return x * x;
+		public static int average(int x, int y) {
+			return (x + y)/2;
 		}
 }
