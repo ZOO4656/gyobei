3730a2510699e2f25e273426fd343d0cd42d951f
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-21 11:09:49 +0900

    練習6-1

--- kita/kita.java
+++ kita/kita.java
@@ -3,14 +3,12 @@ import java.util.Random;
 
 public class Kita{
 	public static void main( String[] args ) throws IOException {
-		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
-		int kuku[][] = new int[9][9];
-		//配列 kuku に値を代入する
-		for( int i = 0 ; i < 9 ; i++) {
-	   	 	for( int j = 0 ; j < 9 ; j++) {
-	   	 		kuku[i][j] = (i+1)*(j+1);
-	   	 		System.out.println(kuku[i][j]);
- 			}
-		}
+		System.out.println("数字を入力してください。その数字を2乗します。");
+		BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(nijo(5));
+
 	}
+		public static int nijo(int x) {
+			return x * x;
+		}
 }
