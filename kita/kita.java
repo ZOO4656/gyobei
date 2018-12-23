d403959f0cbc3e6d5fc6a779de9e572f8d4861ab
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-23 15:59:41 +0900

    練習6-3.2

--- kita/kita.java
+++ kita/kita.java
@@ -1,13 +1,20 @@
 import java.io.*;
 import java.util.Random;
 
-public class Kita{
+public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(max(12,7));
+		System.out.println(max(12,55,34));
 
 	}
-		public static int max(int x, int y) {
-			if(x > y) {return x;}else{return y;}
+	public static int max(int x, int y, int z) {
+		if(x > y && x > z) {
+			return x;
+		}else if(x < y && y > z){
+			return y;
+		}else if (x < z && y < z) {
+			return z;
 		}
+		return x;
+	}
 }
