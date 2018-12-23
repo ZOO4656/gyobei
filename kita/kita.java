c57b472ede3bf9c3a389e820f5b7fdd0948ecb02
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-23 15:38:33 +0900

    練習6-3

--- kita/kita.java
+++ kita/kita.java
@@ -4,10 +4,10 @@ import java.util.Random;
 public class Kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(average(5,7));
+		System.out.println(max(12,7));
 
 	}
-		public static int average(int x, int y) {
-			return (x + y)/2;
+		public static int max(int x, int y) {
+			if(x > y) {return x;}else{return y;}
 		}
 }
