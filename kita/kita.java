bc0a2c4107ac1f89265f74a8c2d2d09d6a93d889
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-26 06:41:05 +0900

    練習6-6

--- kita/kita.java
+++ kita/kita.java
@@ -4,41 +4,11 @@ import java.util.Random;
 public class kita{
 	public static void main( String[] args ) throws IOException {
 		BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
-		doru(3,'$');
-		parcent(5,'%');
-		shapent(7,'#');
+		kuku(3);
 	}
-	public static void doru(int x, char y) {
-		int widhtCounter = 1;
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < widhtCounter; j++) {
-				System.out.print(y);
-			}
-			System.out.println("");
-			widhtCounter++;
+	public static void kuku(int dan) {
+		for(int i = 1; i <= 9; i++) {
+			System.out.print(dan * i + ", ");
 		}
-		System.out.println("");
-	}
-	public static void parcent(int x, char y) {
-		int widhtCounter = 1;
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < widhtCounter; j++) {
-				System.out.print(y);
-			}
-			System.out.println("");
-			widhtCounter++;
-		}
-		System.out.println("");
-	}
-	public static void shapent(int x, char y) {
-		int widhtCounter = 1;
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < widhtCounter; j++) {
-				System.out.print(y);
-			}
-			System.out.println("");
-			widhtCounter++;
-		}
-	System.out.println("");
 	}
 }
