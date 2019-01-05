229b6e2c884ecfd5d78e49521c12bbf8dee9d48e
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-01-05 16:28:55 +0900

    練習7-4

--- kita/Test.java
+++ kita/Test.java
@@ -4,17 +4,24 @@ import java.util.Random;
 public class Test{
 	private String Name;
 	private int Age;
+	private String Kind;
+
+	Test(String kind) {
+		this.Kind = kind;
+	}
 
     public void SetName(String nm ) {
-        Name = nm;
+        this.Name = nm;
     }
 
     public void SetAge(int age) {
-    	Age = age;
+    	this.Age = age;
     }
 
     public void ShowProfile() {
-        System.out.println( "名前は、" + Name + "です。" );
-        System.out.println( "年齢は、" + Age + "歳です。" );
+        System.out.println( "名前は、" + this.Name + "です。" );
+        System.out.println( "年齢は、" + this.Age + "歳です。" );
+        System.out.println( "犬種は、" + this.Kind + "です。" );
+        System.out.println("");
     }
 }
--- kita/kita.java
+++ kita/kita.java
@@ -3,12 +3,12 @@ import java.util.Random;
 
 public class kita{
 	public static void main( String[] args ) throws IOException {
-		Test pomeranian = new Test();
+		Test pomeranian = new Test("ポメラニアン");
 		pomeranian.SetName("coron");
 		pomeranian.SetAge(10);
 		pomeranian.ShowProfile();
 
-		Test pudol = new Test();
+		Test pudol = new Test("プードル");
 		pudol.SetName("maron");
 		pudol.SetAge(15);
 		pudol.ShowProfile();
