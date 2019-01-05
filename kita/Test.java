590e376003b50f15536404452fc9ceab88ea97fa
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-01-05 16:06:08 +0900

    練習7-2

--- kita/Test.java
+++ kita/Test.java
@@ -3,12 +3,18 @@ import java.util.Random;
 
 public class Test{
 	private String Name;
+	private int Age;
 
-    public void SetName( String nm ) {
+    public void SetName(String nm ) {
         Name = nm;
+    }
+
+    public void SetAge(int age) {
+    	Age = age;
     }
 
     public void ShowProfile() {
         System.out.println( "名前は、" + Name + "です。" );
+        System.out.println( "年齢は、" + Age + "歳です。" );
     }
 }
--- kita/kita.java
+++ kita/kita.java
@@ -5,6 +5,7 @@ public class kita{
 	public static void main( String[] args ) throws IOException {
 		Test pomeranian = new Test();
 		pomeranian.SetName("coron");
+		pomeranian.SetAge(10);
 		pomeranian.ShowProfile();
 	}
 }
